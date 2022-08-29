2fd3978172b6e5a3d9eec2545fc489069122bc10
Hojin Ghim <hojin@amazon.com>
2022-08-29 18:57:26 +0100

    Flexible HTML content (functions)
    https://lhbg-book.link/03-html/01-html_content.html

--- hello.hs
+++ hello.hs
@@ -1,2 +1,8 @@
 main = putStrLn myhtml
-myhtml = "<html><body>Hello, world!</body></html>"
+myhtml = makeHtml "My page title" "Hello, world!"
+makeHtml title content =
+    html_ (head_ (title_ title) <> body_ content)
+html_ content = "<html>" <> content <> "</html>"
+head_ content = "<head>" <> content <> "</head>"
+title_ content = "<title>" <> content <> "</title>"
+body_ content = "<body>" <> content <> "</body>"
