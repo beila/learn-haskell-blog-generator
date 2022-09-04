a8ba59e1550b248e096d3dfd11edf8af4712bc85
Hojin Ghim <hojin@amazon.com>
2022-09-04 17:24:05 +0100

    Safer HTML construction with types
    https://lhbg-book.link/03-html/04-safer_construction.html

--- hello.hs
+++ hello.hs
@@ -1,30 +1,40 @@
-main = putStrLn myhtml
+main = putStrLn (render myhtml)
 
-myhtml :: String
-myhtml = makeHtml "My page title" (h1_ "Hello" <> p_ "world")
+myhtml :: Html
+myhtml = html_ "My page title"
+    (append_
+     (h1_ "Hello")
+     (append_
+      (p_ "world")
+      (p_ "or anyone")))
 
-makeHtml :: String -> String -> String
-makeHtml title content =
-    html_ (head_ (title_ title) <> body_ content)
+html_ :: Title -> Structure -> Html
+html_ title body =
+    Html (el "html" ((el "head" (el "title" title)) <> (el "body" (getStructureString body))))
 
-html_ :: String -> String
-html_ = el "html"
+p_ :: String -> Structure
+p_ = Structure . el "p"
 
-head_ :: String -> String
-head_ = el "head"
-
-title_ :: String -> String
-title_ = el "title"
-
-body_ :: String -> String
-body_ = el "body"
-
-p_ :: String -> String
-p_ = el "p"
-
-h1_ :: String -> String
-h1_ = el "h1"
+h1_ :: String -> Structure
+h1_ = Structure . el "h1"
 
 el :: String -> String -> String
 el tag content =
     "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
+newtype Html = Html String
+
+render :: Html -> String
+render (Html html) = html
+
+newtype Structure = Structure String
+
+append_ :: Structure -> Structure -> Structure
+append_ (Structure a) (Structure b) = Structure (a <> b)
+
+getStructureString :: Structure -> String
+getStructureString struct =
+    case struct of
+        Structure str -> str
+
+type Title = String
