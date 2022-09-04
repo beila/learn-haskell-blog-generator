6d9b1add42e5080b9ab3f28a71edbdb8d0324d94
Hojin Ghim <hojin@amazon.com>
2022-09-04 15:33:08 +0100

    Adding type signatures
    https://lhbg-book.link/03-html/02-type_signatures.html#adding-type-signatures

--- hello.hs
+++ hello.hs
@@ -1,8 +1,30 @@
 main = putStrLn myhtml
-myhtml = makeHtml "My page title" "Hello, world!"
+
+myhtml :: String
+myhtml = makeHtml "My page title" (h1_ "Hello" <> p_ "world")
+
+makeHtml :: String -> String -> String
 makeHtml title content =
     html_ (head_ (title_ title) <> body_ content)
-html_ content = "<html>" <> content <> "</html>"
-head_ content = "<head>" <> content <> "</head>"
-title_ content = "<title>" <> content <> "</title>"
-body_ content = "<body>" <> content <> "</body>"
+
+html_ :: String -> String
+html_ = el "html"
+
+head_ :: String -> String
+head_ = el "head"
+
+title_ :: String -> String
+title_ = el "title"
+
+body_ :: String -> String
+body_ = el "body"
+
+p_ :: String -> String
+p_ = el "p"
+
+h1_ :: String -> String
+h1_ = el "h1"
+
+el :: String -> String -> String
+el tag content =
+    "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
