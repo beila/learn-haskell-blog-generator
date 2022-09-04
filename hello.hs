7f87ccd0b10425d45c8e42d33ca5b71bbede48c6
Hojin Ghim <hojin@amazon.com>
2022-09-04 17:31:00 +0100

    Preventing incorrect use with modules
    https://lhbg-book.link/03-html/05-modules.html

--- hello.hs
+++ hello.hs
@@ -1,3 +1,6 @@
+import Html
+
+main :: IO()
 main = putStrLn (render myhtml)
 
 myhtml :: Html
@@ -7,34 +10,3 @@ myhtml = html_ "My page title"
      (append_
       (p_ "world")
       (p_ "or anyone")))
-
-html_ :: Title -> Structure -> Html
-html_ title body =
-    Html (el "html" ((el "head" (el "title" title)) <> (el "body" (getStructureString body))))
-
-p_ :: String -> Structure
-p_ = Structure . el "p"
-
-h1_ :: String -> Structure
-h1_ = Structure . el "h1"
-
-el :: String -> String -> String
-el tag content =
-    "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
-
-newtype Html = Html String
-
-render :: Html -> String
-render (Html html) = html
-
-newtype Structure = Structure String
-
-append_ :: Structure -> Structure -> Structure
-append_ (Structure a) (Structure b) = Structure (a <> b)
-
-getStructureString :: Structure -> String
-getStructureString struct =
-    case struct of
-        Structure str -> str
-
-type Title = String
