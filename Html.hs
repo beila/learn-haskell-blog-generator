6a99ea03015b5f56d322b617a169628c0539bf32
Hojin Ghim <hojin@amazon.com>
2022-10-30 15:40:43 +0000

    Exposing internal functionality (Internal modules)
    https://lhbg-book.link/03-html/07-internal_modules.html

--- Html.hs
+++ Html.hs
@@ -10,47 +10,4 @@ module Html
 )
 where
 
-html_ :: Title -> Structure -> Html
-html_ title body =
-    Html (el "html" ((el "head" (el "title" (escape title))) <> (el "body" (getStructureString body))))
-
-p_ :: String -> Structure
-p_ = Structure . el "p" . escape
-
-h1_ :: String -> Structure
-h1_ = Structure . el "h1" . escape
-
-el :: String -> String -> String
-el tag content =
-    "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
-
-newtype Html = Html String
-
-render :: Html -> String
-render (Html html) = html
-
-newtype Structure = Structure String
-
-append_ :: Structure -> Structure -> Structure
-append_ (Structure a) (Structure b) = Structure (a <> b)
-
-getStructureString :: Structure -> String
-getStructureString struct =
-    case struct of
-        Structure str -> str
-
-type Title = String
-
-escape :: String -> String
-escape =
-    let
-    escapeChar c =
-        case c of
-        '<' -> "&lt;"
-        '>' -> "&gt;"
-        '&' -> "&amp;"
-        '"' -> "&quot;"
-        '\'' -> "&#39;"
-        _ -> [c]
-    in
-    concat . map escapeChar
+import Html.Internal
